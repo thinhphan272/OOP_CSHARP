18232cc04e54a07d46cb04c7d05c4932a2d580eb
thinhphan272 <phanthinh654@gmail.com>
2025-04-14 06:33:17 +0700

    update

---
+++ THINH_OOP/BaiTap3_VeNha/MonHoc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3_VeNha
+{
+    public abstract class MonHoc
+    {
+        protected string maMH;
+        protected string tenMH;
+
+    }
+}
